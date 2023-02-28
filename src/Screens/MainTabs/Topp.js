0018cff7d5be5099c369532723c106614eaf539c
av1roytman <aroytman@purdue.edu>
2023-02-28 17:57:16 -0500

    added video

--- src/Screens/MainTabs/Topp.js
+++ src/Screens/MainTabs/Topp.js
@@ -1,20 +1,60 @@
-import { Text, View, StyleSheet, Button } from "react-native";
+import { Text, View, StyleSheet, Button, SafeAreaView, Pressable } from "react-native";
+import { Video, AVPlaybackStatus } from 'expo-av';
+import { useRef, useState } from "react";
 
-const Topp = ({ navigation }) => {
+const Topp = () => {
+  const video = useRef(null);
+  const [status, setStatus] = useState({});
+
   return (
-    <View style={styles.container}>
-      <Text>This is the Topp page</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topBar}>
+        <Text>This is the Topp page</Text>
+      </View>
+      <View style={styles.body}>
+        <Pressable
+          onPress={() =>
+            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+          }
+        >
+          <Video
+            ref={video}
+            style={styles.video}
+            source={{
+              // uri: 'https://www.youtube.com/watch?v=5gPvi3iXIdM&list=PLxabZQCAe5fjzyawndGLeP1GkJTAjZlKL&index=10'
+              uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+            }}
+            useNativeControls={false}
+            resizeMode="contain"
+            isLooping
+            shouldPlay
+            onPlaybackStatusUpdate={status => setStatus(() => status)}
+          />
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  body: {
+    backgroundColor: "#aff",
+    height: "90%",
+    width: "100%"
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
+  topBar: {
+    backgroundColor: "#bbb",
+    width: "100%",
+    height: "10%"
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+  }
 });
 
 export default Topp;
