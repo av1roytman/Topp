601b8b8f2374ba70c19eab7a4222c6e2880e73de
av1roytman <aroytman@purdue.edu>
2023-03-01 11:49:26 -0500

    added camrea feature

--- src/Screens/MainTabs/Components/VideoBox.js
+++ src/Screens/MainTabs/Components/VideoBox.js
@@ -12,18 +12,18 @@ const VideoBox = ( props ) => {
                 status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
             }
             >
-            <Video
-                ref={video}
-                style={props.styles.video}
-                source={{
-                uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-                }}
-                useNativeControls={false}
-                resizeMode="contain"
-                isLooping
-                shouldPlay
-                onPlaybackStatusUpdate={status => setStatus(() => status)}
-            />
+                <Video
+                    ref={video}
+                    style={props.styles.video}
+                    source={{
+                    uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+                    }}
+                    useNativeControls={false}
+                    resizeMode="contain"
+                    isLooping
+                    shouldPlay
+                    onPlaybackStatusUpdate={status => setStatus(() => status)}
+                />
             </Pressable>
         </View>
     );
