d3c44e03328e175596c732b030bc11795b2cda5b
av1roytman <aroytman@purdue.edu>
2023-03-02 10:26:04 -0500

    fixed camera and added redux for top video to go from the camera button

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import videoReducer from "./videoSlice";
 
 export default configureStore({
-  reducer: { user: userReducer },
+  reducer: { 
+    user: userReducer,
+    video: videoReducer,
+  },
 });
